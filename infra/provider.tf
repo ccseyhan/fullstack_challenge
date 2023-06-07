a25d22ba7d1802ec1e1bfb89d6bf118620859096
ccseyhan <cemalcan.seyhan@deop.ca>
2023-06-07 12:38:15 -0300

    devel added

---
+++ infra/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.59.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "sshkey"
+    storage_account_name = "ccseyhan"
+    container_name       = "fullstack-terraform-backend"
+    key                  = "prodgit sstat.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+    features {
+      
+    }
+  # Configuration options
+}
